d33ff5ca4f0de848d86911e2cdec99ff6f8123d7
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2017-12-16 10:13:21 +0900

    pacman config

---
+++ 00_pacman/main.df.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# dotfiles
+# Copyright (C) 2017  Kazutoshi Noguchi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pacman_install patch
+pacman_install rsync
+
+# enable multilib repo
+patch /etc/pacman.conf pacman.conf.patch
+pacman_conf_changed=$changed
+
+copy --chown root:root --chmod 0644 mirrorlist /etc/pacman.d/mirrorlist
+mirrorlist_changed=$changed
+
+if ! is_dry_run && (( pacman_conf_changed || mirrorlist_changed )); then
+    pacman_update
+fi
