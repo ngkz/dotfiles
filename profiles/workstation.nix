02a2cd96ab3ba99fa363cb60fffb49e394aa5312
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2021-07-04 00:59:40 +0900

    xdg-desktop-portal

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -105,6 +105,15 @@
     wrapperFeatures.gtk = true;
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+    gtkUsePortal = true;
+  };
+
   environment.systemPackages = with pkgs; [
     git
     binutils
