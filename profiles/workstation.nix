36aba7a5d336b647288bd0be811d4fed377b8948
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2021-07-03 18:37:26 +0900

    CUPS and PipeWire

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -2,6 +2,18 @@
 
 { config, pkgs, ... }:
 {
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable PipeWire sound daemon.
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   # mDNS
   services.avahi = {
     enable = true;
