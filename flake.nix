668d62228489bffb0e0e3d6b1f7a2cc7219778b2
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2021-07-12 16:56:29 +0900

    make nixos-unstable packages accessible

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-21.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     # TODO use upstream agenix after https://github.com/ryantm/agenix/pull/49 merge
     agenix.url = "github:ngkz/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
@@ -12,14 +13,18 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, agenix, flake-utils, home-manager }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, agenix, flake-utils, home-manager }:
     let
+      # make nixos-unstable packages accessible through pkgs.unstable.package
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable { inherit (prev) system; };
+      };
+
       overlays = [
         agenix.overlay # add agenix package
+        overlay-unstable
       ];
     in {
-      inherit overlays;
-
       # Used with `nixos-rebuild --flake .#<hostname>`
       nixosConfigurations = {
         stagingvm = nixpkgs.lib.nixosSystem {
@@ -27,6 +32,7 @@
           modules = [
             # https://nixos.org/manual/nixos/stable/index.html#sec-configuration-syntax
             # https://nixos.org/manual/nixos/stable/index.html#sec-writing-modules
+            { nixpkgs.overlays = overlays; }
             ./configuration.nix
             hosts/stagingvm/configuration.nix
             profiles/ssd.nix
