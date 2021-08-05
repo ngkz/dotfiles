a309b85e6f5575ea9ad061dac5012f26240fda6e
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2021-08-05 21:48:16 +0900

    add nixos-hardware flake

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager/release-21.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
   outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, agenix, flake-utils, home-manager }: {
