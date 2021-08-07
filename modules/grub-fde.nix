c16bb196a2b8d23523bc3386109cadfb358fa559
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2021-08-07 12:20:06 +0900

    ask cryptomount password multiple times

--- modules/grub-fde.nix
+++ modules/grub-fde.nix
@@ -1,7 +1,7 @@
 # GRUB + Full Disk Encryption including /boot + Secure Boot (WIP)
-{ config, lib, inputs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 let
-  inherit (lib) mkOption types;
+  inherit (lib) mkOption types escapeShellArg replaceChars;
 in
 {
   options.modules.grub-fde.cryptlvmDevice = mkOption {
@@ -12,23 +12,24 @@ in
   config =
     let
       cfg = config.modules.grub-fde;
+      grub = pkgs.grub2.override { efiSupport = true; };
+      realBoot = "${config.modules.tmpfs-as-root.storage}/boot";
+      espMount = "${realBoot}/efi";
+      bootloaderId = "NixOS" + (replaceChars [ "/" ] [ "-" ] espMount);
     in
     {
       modules.tmpfs-as-root.persistentDirs = [
         "/boot"
       ];
 
-      fileSystems."/nix/persist/boot/efi" = {
+      fileSystems."${espMount}" = {
         label = "ESP";
         fsType = "vfat";
       };
 
       boot = {
         loader = {
-          efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/nix/persist/boot/efi"; # ESP mount point
-          };
+          efi.canTouchEfiVariables = true;
 
           grub = {
             enable = true;
@@ -37,10 +38,40 @@ in
             # install-grub.pl goes bananas if /boot or /nix are bind-mount or symlink
             # Relocate /boot to /nix/persist/boot
             mirroredBoots = [{
-              path = "/nix/persist/boot";
+              path = realBoot;
               devices = [ "nodev" ];
-              efiSysMountPoint = config.boot.loader.efi.efiSysMountPoint;
+              efiSysMountPoint = espMount;
             }];
+            extraInstallCommands = ''
+              grub_tmp=$(mktemp -d -t grub.conf.XXXXXXXX)
+              trap 'rm -rf -- "$grub_tmp"' EXIT
+
+              cat <<EOS >"$grub_tmp/grub.cfg"
+                # Ask the password multiple times
+                while ! cryptomount -u $(grub-probe -t cryptodisk_uuid ${escapeShellArg realBoot}); do
+                  true
+                done
+
+                set root='$(${grub}/bin/grub-probe -t drive ${escapeShellArg realBoot} | ${pkgs.gnused}/bin/sed 's/^(\(.*\))$/\1/')'
+                set prefix='$(${grub}/bin/grub-probe -t drive ${escapeShellArg realBoot})$(${grub}/bin/grub-mkrelpath ${escapeShellArg realBoot}/grub)'
+                configfile \$prefix/grub.cfg
+
+                # Prevent dropping to a rescue shell
+                echo "Boot failed. Rebooting the system in 10 seconds."
+                sleep 10
+                reboot
+              EOS
+
+              ${grub}/bin/grub-mkstandalone \
+                --format=${grub.grubTarget} \
+                --modules "part_$(${grub}/bin/grub-probe -t partmap ${escapeShellArg realBoot})
+                           $(${grub}/bin/grub-probe -t abstraction ${escapeShellArg realBoot})
+                           $(${grub}/bin/grub-probe -t fs ${escapeShellArg espMount})
+                           $(${grub}/bin/grub-probe -t fs ${escapeShellArg realBoot})
+                           configfile true echo sleep reboot" \
+                --output ${escapeShellArg "${espMount}/EFI/${bootloaderId}"}/grub*.efi \
+                "boot/grub/grub.cfg=$grub_tmp/grub.cfg"
+            '';
           };
         };
 
@@ -56,7 +87,7 @@ in
           };
 
           secrets = {
-            "cryptlvm.key" = "/nix/persist/secrets/cryptlvm.key";
+            "cryptlvm.key" = "${config.modules.tmpfs-as-root.storage}/secrets/cryptlvm.key";
           };
         };
       };
