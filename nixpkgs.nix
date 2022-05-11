18d5dd073d9937b597263714bce1bf10dcfd45df
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-05-12 03:27:47 +0900

    forgot to commit nixpkgs.nix change

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,15 @@
 # Nixpkgs config
 { inputs }:
 let
-  inherit (inputs) nixpkgs-unstable agenix devshell;
+  inherit (inputs) nixpkgs-unstable agenix devshell self;
 
   # make nixos-unstable packages accessible through pkgs.unstable.package
   overlay-unstable = final: prev: {
     unstable = import nixpkgs-unstable { inherit (prev) system; };
+  };
+
+  overlay-my-packages = final: prev: {
+    my = self.packages.${prev.system};
   };
 in
 {
@@ -13,6 +17,7 @@ in
     agenix.overlay # add agenix package
     devshell.overlay
     overlay-unstable
+    overlay-my-packages
   ];
   config.allowUnfree = true;
 }
