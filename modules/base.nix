8670c2500f3ef1109a438bddf078586f6bb85813
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-05-24 14:57:04 +0900

    Add KEY_ESC to EV_KEY

--- modules/base.nix
+++ modules/base.nix
@@ -140,7 +140,7 @@ in
       - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
         DEVICE:
           EVENTS:
-            EV_KEY: [KEY_CAPSLOCK]
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
     '';
   };
 
