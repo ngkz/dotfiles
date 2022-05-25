75b777e204ae4a034c59b49ffd85c53db41a91ab
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-05-25 22:25:27 +0900

    sway-systemd update script

---
+++ packages/sway-systemd/update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S nix shell nixpkgs#bash nixpkgs#curl nixpkgs#nodePackages.node2nix nixpkgs#nix-prefetch-github nixpkgs#jq -c bash
+set -euo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+pname=sway-systemd
+owner=alebastr
+repo=$pname
+
+current=$(nix eval --raw "../..#${pname}.version")
+latest_tag=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest" | jq -r ".tag_name")
+latest=$(echo "$latest_tag" | cut -c2-)
+
+if [[ $current == $latest ]]; then
+    echo "$pname is up-to-date: $latest"
+    exit 0
+fi
+
+sed -i "s/version = \"$current\"/version = \"$latest\"/" default.nix
+newhash=$(nix-prefetch-github --json "$owner" "$repo" --rev "$latest_tag" | jq -r .sha256)
+sed -i "s|sha256 = \".*\"|sha256 = \"$newhash\"|" default.nix
+
+echo "$pname updated: $current -> $latest"
