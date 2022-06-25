7ca47aedffe40dd38a6523286979516b254b6a91
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-06-25 13:34:36 +0900

    improve nodePackages update script

--- packages/nodePackages/update.sh
+++ packages/nodePackages/update.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S nix shell nixpkgs#bash nixpkgs#nodePackages.node2nix nixpkgs#nixpkgs-fmt -c bash
 set -eu -o pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-nix run nixpkgs#nodePackages.node2nix -- \
+node2nix \
     -i node-packages.json \
     --pkg-name nodejs-14_x #XXX workaround for node2nix#236
+
+nixpkgs-fmt .
