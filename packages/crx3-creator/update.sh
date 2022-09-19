8d012e97735650f9aa8856a7bc914404542ac4c0
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-09-19 13:04:42 +0900

    crx3-creator update script

---
+++ packages/crx3-creator/update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S nix shell nixpkgs#bash nixpkgs#curl nixpkgs#nix-prefetch-github nixpkgs#jq -c bash
+set -euo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+pname=crx3-creator
+owner=pawliczka
+repo=CRX3-Creator
+
+current=$(nix eval --raw "../..#${pname}.src.rev")
+commit=$(curl -s https://api.github.com/repos/$owner/$repo/commits/master)
+latest=$(jq -r .sha <<<"$commit")
+
+if [[ $current == "$latest" ]]; then
+    echo "$pname is up-to-date: $latest"
+    exit 0
+fi
+
+date=$(jq -r .commit.committer.date <<<"$commit")
+version=unstable-$(date +%Y-%m-%d --date="$date")
+newhash=$(nix-prefetch-github --json "$owner" "$repo" --rev "$latest" | jq -r .sha256)
+sed -i "s/version = \".*\"/version = \"$version\"/" default.nix
+sed -i "s|rev = \".*\"|rev = \"$latest\"|" default.nix
+sed -i "s|sha256 = \".*\"|sha256 = \"$newhash\"|" default.nix
+
+echo "$pname updated: $current -> $latest ($version)"
