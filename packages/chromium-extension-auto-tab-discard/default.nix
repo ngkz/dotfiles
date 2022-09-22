a11670aac21041c4b881f7ec1a5fd0a1d1eb4a33
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-09-22 12:25:13 +0900

    update

--- packages/chromium-extension-auto-tab-discard/default.nix
+++ packages/chromium-extension-auto-tab-discard/default.nix
@@ -1,10 +1,10 @@
 { fetchzip, ngkz, lib, ... }:
 ngkz.buildChromiumExtension rec {
   pname = "auto-tab-discard";
-  version = "0.6.3";
+  version = "0.6.3.2";
   src = fetchzip {
     url = "https://github.com/rNeomy/auto-tab-discard/archive/refs/tags/${version}.zip";
-    sha256 = "1ppvxha4l2vivv9v44sy4lhw53xy4kr66f97sg2ncnpig4181jgg";
+    sha256 = "0z301z8vxq9ica6z4nmi5bffan8bxv6668f5fs7vrb5x5h0s5j1i";
   };
   sourceRoot = "source/v3";
 }
