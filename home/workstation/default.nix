31267268788a6c50e7807fcb5878755d8e9fe293
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-10-22 13:08:34 +0900

    replace zathura with evince

--- home/workstation/default.nix
+++ home/workstation/default.nix
@@ -126,8 +126,6 @@ in
     };
   };
 
-  programs.zathura.enable = true; #PDF viewer
-
   #nm-applet
   services.network-manager-applet.enable = true;
   xsession.preferStatusNotifierItems = true;
@@ -314,13 +312,39 @@ in
       # Nemo
       "inode/directory" = "nemo.desktop";
 
-      # Zathura
-      "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
-      "application/postscript" = "org.pwmt.zathura-ps.desktop";
-      "application/eps" = "org.pwmt.zathura-ps.desktop";
-      "application/x-eps" = "org.pwmt.zathura-ps.desktop";
-      "image/eps" = "org.pwmt.zathura-ps.desktop";
-      "image/x-eps" = "org.pwmt.zathura-ps.desktop";
+      # Evince
+      "application/vnd.comicbook-rar" = "org.gnome.Evince.desktop";
+      "application/vnd.comicbook+zip" = "org.gnome.Evince.desktop";
+      "application/x-cb7" = "org.gnome.Evince.desktop";
+      "application/x-cbr" = "org.gnome.Evince.desktop";
+      "application/x-cbt" = "org.gnome.Evince.desktop";
+      "application/x-cbz" = "org.gnome.Evince.desktop";
+      "application/x-ext-cb7" = "org.gnome.Evince.desktop";
+      "application/x-ext-cbr" = "org.gnome.Evince.desktop";
+      "application/x-ext-cbt" = "org.gnome.Evince.desktop";
+      "application/x-ext-cbz" = "org.gnome.Evince.desktop";
+      "application/x-ext-djv" = "org.gnome.Evince.desktop";
+      "application/x-ext-djvu" = "org.gnome.Evince.desktop";
+      "image/vnd.djvu" = "org.gnome.Evince.desktop";
+      "application/x-bzdvi" = "org.gnome.Evince.desktop";
+      "application/x-dvi" = "org.gnome.Evince.desktop";
+      "application/x-ext-dvi" = "org.gnome.Evince.desktop";
+      "application/x-gzdvi" = "org.gnome.Evince.desktop";
+      "application/pdf" = "org.gnome.Evince.desktop";
+      "application/x-bzpdf" = "org.gnome.Evince.desktop";
+      "application/x-ext-pdf" = "org.gnome.Evince.desktop";
+      "application/x-gzpdf" = "org.gnome.Evince.desktop";
+      "application/x-xzpdf" = "org.gnome.Evince.desktop";
+      "application/postscript" = "org.gnome.Evince.desktop";
+      "application/x-bzpostscript" = "org.gnome.Evince.desktop";
+      "application/x-gzpostscript" = "org.gnome.Evince.desktop";
+      "application/x-ext-eps" = "org.gnome.Evince.desktop";
+      "application/x-ext-ps" = "org.gnome.Evince.desktop";
+      "image/x-bzeps" = "org.gnome.Evince.desktop";
+      "image/x-eps" = "org.gnome.Evince.desktop";
+      "image/x-gzeps" = "org.gnome.Evince.desktop";
+      "application/oxps" = "org.gnome.Evince.desktop";
+      "application/vnd.ms-xpsdocument" = "org.gnome.Evince.desktop";
 
       # mpv
       "application/ogg" = "mpv.desktop";
@@ -524,5 +548,6 @@ in
     gnome.totem #audio/video thumbnailer
     gnome.gnome-disk-utility
     gnome.file-roller
+    gnome.evince #pdf viewer/thumbnailer
   ];
 }
