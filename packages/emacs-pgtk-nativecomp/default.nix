376593253f72cece734bb48dacfa044f1b183d20
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-11-04 16:01:10 +0900

    fix emacs native compilation issue

--- packages/emacs-pgtk-nativecomp/default.nix
+++ packages/emacs-pgtk-nativecomp/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, emacs, fetchFromGitHub }: (emacs.override {
+{ stdenv, emacs, fetchFromGitHub, lib, libgccjit }: (emacs.override {
   inherit stdenv; # make stdenv overridable
   withPgtk = true;
   nativeComp = true;
@@ -12,4 +12,29 @@
     sha256 = "2ojKi5x66wUXTNvMbZOdJwVZIQ9FgLQs7+JNm5GjzLU=";
   };
   patches = [ ];
+  # from: https://github.com/nix-community/emacs-overlay/blob/c1143602cb97fa717bacdf5668ceca1bb487918d/overlays/emacs.nix
+  # XXX: remove when https://github.com/NixOS/nixpkgs/pull/193621 is merged
+  postPatch = previousAttrs.postPatch +
+    (lib.optionalString (previousAttrs ? NATIVE_FULL_AOT)
+      (
+        let backendPath = (lib.concatStringsSep " "
+          (builtins.map (x: ''\"-B${x}\"'') [
+            # Paths necessary so the JIT compiler finds its libraries:
+            "${lib.getLib libgccjit}/lib"
+            "${lib.getLib libgccjit}/lib/gcc"
+            "${lib.getLib stdenv.cc.libc}/lib"
+
+            # Executable paths necessary for compilation (ld, as):
+            "${lib.getBin stdenv.cc.cc}/bin"
+            "${lib.getBin stdenv.cc.bintools}/bin"
+            "${lib.getBin stdenv.cc.bintools.bintools}/bin"
+          ]));
+        in
+        ''
+          substituteInPlace lisp/emacs-lisp/comp.el --replace \
+              "(defcustom comp-libgccjit-reproducer nil" \
+              "(setq native-comp-driver-options '(${backendPath}))
+          (defcustom comp-libgccjit-reproducer nil"
+        ''
+      ));
 })
