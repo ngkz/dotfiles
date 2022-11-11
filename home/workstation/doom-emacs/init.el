4750c143c562e179e8c35ff65eea4e95e791444e
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-11-11 11:26:00 +0900

    change doom emacs config

--- home/workstation/doom-emacs/init.el
+++ home/workstation/doom-emacs/init.el
@@ -153,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        (nix +tree-sitter +lsp) ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +dragndrop +gnuplot +hugo +journal +pretty) ; organize your plain life in plain text
+       (org +dragndrop +gnuplot +pretty +pomodoro) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
