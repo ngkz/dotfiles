ca503ddd47ce39b1b8e9c93f1747117c1891741e
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-11-11 11:25:31 +0900

    apply home.sessionpath when logined via ssh

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -152,6 +152,9 @@ in
     '';
   };
 
+  # XXX Apply home.sessionPath when logined via ssh
+  programs.zsh.enable = true;
+
   # XXX Apply home.sessionVariables. Workaround for home-manager #1011
   environment.extraInit = ''
     if [ -e "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh" ]; then
