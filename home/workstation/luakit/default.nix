2c3119c7565eae2f4a2c818c4eceda3b04156686
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-11-16 21:07:01 +0900

    update

--- home/workstation/luakit/default.nix
+++ home/workstation/luakit/default.nix
@@ -10,19 +10,19 @@
 
   home.file.".local/share/luakit/adblock/easylist.txt".source = pkgs.fetchurl {
     url = "https://easylist.to/easylist/easylist.txt";
-    sha256 = "0259hqwyrp003awf8z69j5gy1lw6qkymcmmgsc52qmm53x409y90";
+    sha256 = "00f52b7f6ss06pksk534xxj5hldx4ivrjnpddn660wsp9bvxm3qv";
   };
   home.file.".local/share/luakit/adblock/easyprivacy.txt".source = pkgs.fetchurl {
     url = "https://easylist.to/easylist/easyprivacy.txt";
-    sha256 = "1nav511476c3yv01ndfyms815y7vs1smwrr8lr1n2vyxqbwkpn26";
+    sha256 = "0q9l9dc3fhl5xbm7r00wdhfd4gayxvjgaa5994277iidfiw3ycw3";
   };
   home.file.".local/share/luakit/adblock/fanboy-cookiemonster.txt".source = pkgs.fetchurl {
     url = "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt";
-    sha256 = "0im9l5s3bmygcjgrbmj4wkicdz4r5g8x93qfkx3sc1dq8ycp4l6n";
+    sha256 = "0a97zr57wm6jsbp9g6806j4j1fmspi3grp930b12mnrydy1w1ch7";
   };
   home.file.".local/share/luakit/adblock/adguard-japanese-filter.txt".source = pkgs.fetchurl {
     url = "https://filters.adtidy.org/extension/ublock/filters/7.txt";
-    sha256 = "05hf23099g5a577n5dm0r64a8n7irzaknadciw9nkccbnvywgpk6";
+    sha256 = "04w22jx23b9h454bw312ql434rhsmqs72qzin24vgkv1iw2a41af";
   };
 
   home.file.".local/share/luakit/bookmarks.db".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/misc/otg/luakit-bookmarks.db";
