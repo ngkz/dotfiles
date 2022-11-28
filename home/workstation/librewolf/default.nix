ce89801b685bb94c670ba9dc730d0673e504bd92
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-11-28 21:00:18 +0900

    configure librewolf

--- home/workstation/librewolf/default.nix
+++ home/workstation/librewolf/default.nix
@@ -1,11 +1,41 @@
 { pkgs, ... }: {
-  #TODO: media.ffmpeg.vaapi.enabled
-
-  home.packages = with pkgs; [
-    librewolf
+  imports = [
+    ./hm-librewolf.nix #XXX remove this after 22.11 upgrade
   ];
 
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      # enable hardware video decoding
+      "media.ffmpeg.vaapi.enable" = true;
+      # enable firefox sync
+      "identity.fxaccounts.enabled" = true;
+      # remember history
+      "privacy.clearOnShutdown.cache" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "privacy.clearOnShutdown.downloads" = false;
+      "privacy.clearOnShutdown.formdata" = false;
+      "privacy.clearOnShutdown.offlineApps" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.sessions" = false;
+      # turn off delete cookies and site data when librewolf is closed
+      "privacy.sanitize.sanitizeOnShutdown" = false;
+      # turn on confirm before closing multiple tabs
+      "browser.tabs.warnOnClose" = true;
+      # turn on open previous windows and tabs
+      "browser.startup.page" = 3;
+      # disable RFP
+      "privacy.resistFingerprinting" = false;
+    };
+  };
+
+  home.file.".librewolf/profiles.ini".source = ./profiles.ini;
+  home.file.".librewolf/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".source = pkgs.substituteAll {
+    src = ./org.keepassxc.keepassxc_browser.json;
+    inherit (pkgs) keepassxc;
+  };
+
   home.tmpfs-as-home.persistentDirs = [
-    ".librewolf"
+    ".librewolf/default"
   ];
 }
