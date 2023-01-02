571037d09f849e32470fa4a496bb1eb6f8452a93
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-01-02 20:42:54 +0900

    fix workspace switcher bug

--- packages/hotkey-scripts/workspace.sh
+++ packages/hotkey-scripts/workspace.sh
@@ -1,29 +1,46 @@
 #!@bash@/bin/bash
-# XMonad-like workspace switching
+# xmonad-style workspace switching
 set -euo pipefail
 PATH=@coreutils@/bin:@jq@/bin:@sway@/bin:@gnused@/bin
 
-workspace_want=$1
+workspace_want_name=$1
 workspaces=$(swaymsg -t get_workspaces)
 
-ws_num() {
+ws_num_from_name() {
     sed -En 's/([0-9]+)(:.*)?/\1/p' <<<"$1"
 }
 
-ws_output() {
-    jq -r ".[]|select(.num == $(ws_num "$1")).output" <<<"$workspaces"
-}
+focused_workspace=$(jq '.[]|select(.focused)' <<<"$workspaces")
+focused_workspace_num=$(jq -r '.num' <<<"$focused_workspace")
+focused_output=$(jq -r '.output' <<<"$focused_workspace")
+workspace_want_num=$(ws_num_from_name "$workspace_want_name")
 
-focused_workspace=$(jq -r '.[]|select(.focused).name' <<<"$workspaces")
-active_output=$(ws_output "$focused_workspace")
-workspace_want_output=$(ws_output "$workspace_want")
-
-if [[ $(ws_num "$focused_workspace") = $(ws_num "$workspace_want") ]]; then
-    exit 0
+if [[ $focused_workspace_num = $workspace_want_num ]]; then
+    exit
 fi
 
-swaymsg workspace number "$workspace_want"
+workspace_want=$(jq ".[]|select(.num == $workspace_want_num)" <<<"$workspaces")
+workspace_want_output=$(jq -r ".output" <<<"$workspace_want")
 
-if [[ -n $workspace_want_output ]] && [[ $workspace_want_output != $active_output ]]; then
-    swaymsg move workspace to output "$active_output"
+if [[ -z $workspace_want_output ]] || [[ $workspace_want_output = $focused_output ]]; then
+    # workspace is on focused output or does not exist: just switch
+    swaymsg workspace number "$workspace_want_name"
+    exit
 fi
+
+workspace_want_visible=$(jq ".visible" <<<"$workspace_want")
+
+if [[ $workspace_want_visible != true ]]; then
+    # workspace is on other output and hidden
+    swaymsg workspace number "$workspace_want_name"
+    swaymsg move workspace to output "$focused_output"
+    exit
+fi
+
+# workspace is on other output and visible
+# swap workspaces
+swaymsg workspace number "$workspace_want_name"
+swaymsg move workspace to output "$focused_output"
+swaymsg workspace number "$focused_workspace_num"
+swaymsg move workspace to output "$workspace_want_output"
+swaymsg focus output "$focused_output"
