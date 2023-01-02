a04ff00a357ed1433fefae1f4362f3e1f6a17786
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-01-02 18:04:22 +0900

    add workaround for kanshi bug

--- home/sway-desktop/default.nix
+++ home/sway-desktop/default.nix
@@ -205,8 +205,8 @@ in
       # XXX workaround for home-manager #2806
       exec ${pkgs.ngkz.sway-systemd}/libexec/sway-systemd/session.sh --with-cleanup --add-env=SDL_VIDEODRIVER --add-env=_JAVA_AWT_WM_NONREPARENTING --add-env=CLUTTER_BACKEND --add-env=MOZ_ENABLE_WAYLAND --add-env=NIXOS_OZONE_WL --add-env=GTK_IM_MODULE  --add-env=QT_IM_MODULE --add-env=XMODIFIERS
 
-      # initial workspace
-      workspace number 1
+      # XXX workaround for kanshi #35
+      exec_always systemctl restart --user kanshi
     '';
   };
 
