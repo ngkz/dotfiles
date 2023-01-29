609bf7fe5e14d9ba8bcfd3bed70c088f09656728
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-01-29 16:08:14 +0900

    set cpu scaling governor

--- hosts/peregrine/default.nix
+++ hosts/peregrine/default.nix
@@ -130,6 +130,9 @@ in
     PLATFORM_PROFILE_ON_AC = "performance";
     PLATFORM_PROFILE_ON_BAT = "low-power";
 
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
     CPU_ENERGY_PERF_POLICY_ON_AC = "performance"; # to achieve 4.6GHz single-core boost clock
     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
     CPU_HWP_DYN_BOOST_ON_AC = 1;
