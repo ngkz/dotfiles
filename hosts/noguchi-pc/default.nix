e93c1f11f109bd0b822e91c36897bdbb851b7b2e
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-02-01 12:11:40 +0900

    hosts/noguchi-pc: set cpu scaling governor

--- hosts/noguchi-pc/default.nix
+++ hosts/noguchi-pc/default.nix
@@ -121,6 +121,9 @@ in
   services.tlp.settings = {
     DISK_DEVICES = "sda";
 
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
     CPU_HWP_DYN_BOOST_ON_AC = 1;
