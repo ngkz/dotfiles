374683012d1cdd6347a4506d86fcd170045960f8
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-02-22 16:19:49 +0900

    extract theming module

--- home/default.nix
+++ home/default.nix
@@ -3,4 +3,5 @@
   tmpfs-as-home = import ./tmpfs-as-home.nix;
   workstation = import ./workstation;
   sway-desktop = import ./sway-desktop;
+  theming = import ./theming;
 }
--- home/workstation/default.nix
+++ home/workstation/default.nix
@@ -1,13 +1,7 @@
 { lib, config, pkgs, ... }:
 let
   inherit (lib.ngkz) rot13;
-  iniFormat = pkgs.formats.ini { };
   canberra-gtk-play = "${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play";
-  vimix-theme = pkgs.vimix-gtk-themes.override {
-    themeVariants = [ "ruby" ];
-    colorVariants = [ "dark" ];
-    sizeVariants = [ "compact" ];
-  };
 in
 {
   imports = [
@@ -16,6 +10,7 @@ in
     ./ungoogled-chromium.nix
     ./syncthing.nix
     ./librewolf
+    ../theming
   ];
 
   # XDG user dirs
@@ -613,62 +608,6 @@ in
     "org/nemo/window-state" = {
       sidebar-bookmark-breakpoint = 8;
     };
-
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark"; # GTK4 dark theme
-      enable-animations = true;
-      document-font-name = "Sans Serif 9";
-      monospace-font-name = "Monospace 9";
-    };
-
-    "org/gnome/desktop/wm/preferences" = {
-      button-layout = ":"; # hide window buttons
-    };
-  };
-
-  gtk = {
-    enable = true;
-    cursorTheme = {
-      package = pkgs.bibata-extra-cursors;
-      name = "Bibata-Original-DarkRed";
-    };
-    font = {
-      name = "Sans-Serif";
-      size = 9;
-    };
-    theme = {
-      package = vimix-theme;
-      name = "vimix-dark-compact-ruby";
-    };
-    iconTheme = {
-      package = pkgs.vimix-icon-theme;
-      name = "Vimix-Ruby-dark";
-    };
-    gtk3.bookmarks = [
-      "file:///home/user/docs docs"
-      "file:///home/user/pics pics"
-      "file:///home/user/music music"
-      "file:///home/user/videos videos"
-      "file:///home/user/dl dl"
-      "file:///home/user/projects projects"
-      "file:///home/user/work work"
-      "file:///home/user/misc misc"
-    ];
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
-  };
-
-  xdg.configFile."gtk-4.0/assets".source = "${vimix-theme}/share/themes/vimix-dark-compact-ruby/gtk-4.0/assets";
-  xdg.configFile."gtk-4.0/gtk.css".source = "${vimix-theme}/share/themes/vimix-dark-compact-ruby/gtk-4.0/gtk.css";
-  xdg.configFile."gtk-4.0/gtk-dark.css".source = "${vimix-theme}/share/themes/vimix-dark-compact-ruby/gtk-4.0/gtk-dark.css";
-
-  home.sessionVariables.QT_QPA_PLATFORMTHEME = "qt5ct";
-  xdg.configFile."qt5ct/qt5ct.conf".source = ./qt5ct.conf;
-
-  xdg.configFile."Kvantum/VimixRuby".source ="${pkgs.ngkz.vimix-kde}/share/Kvantum/VimixRuby";
-  xdg.configFile."Kvantum/kvantum.kvconfig".source = iniFormat.generate "kvantum.kvconfig" {
-    General.theme = "VimixRubyDark";
   };
 
   home.packages = with pkgs; [
@@ -705,8 +644,6 @@ in
     tpm2-tools
     efitools
 
-    libsForQt5.qt5ct
-    libsForQt5.qtstyleplugin-kvantum
     gnome.dconf-editor
     gnome.gnome-font-viewer
     ngkz.freecad-realthunder
