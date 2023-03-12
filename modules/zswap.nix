78a4c05d558c547562a441f10921e92e27af9d8d
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-03-12 22:15:47 +0900

    zswap config not applied on some machines

--- modules/zswap.nix
+++ modules/zswap.nix
@@ -2,11 +2,13 @@
   # compress memory and store in RAM before swapping to disk
   boot.kernelParams = [
     "zswap.enabled=1"
-    "zswap.compressor=lz4"
-    "zswap.zpool=z3fold" # max 3:1 compression ratio
   ];
   boot.initrd.kernelModules = [
     "lz4"
     "z3fold"
   ];
+  boot.initrd.preDeviceCommands = ''
+    echo lz4 >/sys/module/zswap/parameters/compressor
+    echo z3fold >/sys/module/zswap/parameters/zpool
+  '';
 }
