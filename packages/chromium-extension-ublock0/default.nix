d926d234f57630dd75e94305ccfe2abe6450dd3f
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-06-08 15:26:44 +0900

    update

--- packages/chromium-extension-ublock0/default.nix
+++ packages/chromium-extension-ublock0/default.nix
@@ -1,9 +1,9 @@
 { fetchzip, ngkz, lib, ... }:
 ngkz.buildChromiumExtension rec {
   pname = "ublock0";
-  version = "1.49.2";
+  version = "1.50.0";
   src = fetchzip {
     url = "https://github.com/gorhill/uBlock/releases/download/${version}/uBlock0_${version}.chromium.zip";
-    sha256 = "1v7lyz183dj59gcrbnr1k9l0l6i2iqcpqp8hga6gi5s0q8015dp6";
+    sha256 = "054s6n6x5y1lplcqxmnis6r9iiibia1by2d60ba8g39r2vnf239w";
   };
 }
