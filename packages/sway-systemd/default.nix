6e3d7ff097492c10b8613cf4d2ccb08e8d704268
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-06-21 13:44:30 +0900

    update

--- packages/sway-systemd/default.nix
+++ packages/sway-systemd/default.nix
@@ -15,44 +15,26 @@
 , makeWrapper
 , python3
 , wrapPython
-, cgroups ? false
-, autostart ? false
-, locale1 ? false
 , ...
 }:
 
 stdenv.mkDerivation rec {
   pname = "sway-systemd";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "alebastr";
     repo = pname;
     rev = "v${version}";
-    sha256 = "Azy7XRHrKvhODxAogwtk2+W0WjGcoTy47+nT0x9aMPw=";
+    sha256 = "wznYE1/lVJtvf5Nq96gbPYisxc2gWLahVydwcH1vwoQ=";
   };
-
-  patches = [
-    ./stop-graphical-session.patch
-  ];
-
-  mesonFlags = lib.optionals cgroups [
-    "-Dcgroups=enabled"
-  ] ++ lib.optionals autostart [
-    "-Dautostart=true"
-  ] ++ lib.optionals locale1 [
-    "-Dlocale1=true"
-  ];
 
   PKG_CONFIG_SYSTEMD_SYSTEMDUSERUNITDIR = "${placeholder "out"}/lib/systemd/user";
 
   pythonPath = [
     dbus-next
-  ] ++ lib.optionals (cgroups || locale1) [
     i3ipc
-  ] ++ lib.optionals (cgroups || autostart) [
     tenacity
-  ] ++ lib.optionals cgroups [
     psutil
     xlib
   ];
