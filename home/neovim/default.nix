01439dd4872b5990e1a5199f936d79a8af8cd08e
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-07-20 15:53:48 +0900

    install capture.vim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -88,6 +88,15 @@
         plugin = which-key-nvim;
       }
       # nvim-treesitter
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "capture-vim";
+        src = pkgs.fetchFromGitHub {
+          owner = "tyru";
+          repo = "capture.vim";
+          rev = "857ee11cfe1193948d3d45dcb8d511fded8533fb";
+          sha256 = "nYMNXdHFVaw6cFKGT9KiHzrlQ7u76WdA4UlvaII+bok=";
+        };
+      })
     ];
     extraPackages = with pkgs; [
       fzf
