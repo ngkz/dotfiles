39bd592db01961ff4482eef79555c649162993c3
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-07-21 03:14:27 +0900

    make trailing space highlight work after session load

--- home/neovim/nvim/lua/config/base16.lua
+++ home/neovim/nvim/lua/config/base16.lua
@@ -1,5 +1,10 @@
 vim.g.base16colorspace = 256
 
+local function add_match()
+    -- highlight trailing spaces
+    vim.cmd.match("ExtraWhitespace", '"\\s\\+$"')
+end
+
 local group = vim.api.nvim_create_augroup("on_change_colorscheme", {})
 vim.api.nvim_create_autocmd("ColorScheme", {
     group = group,
@@ -14,15 +19,13 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
         -- highlight trailing spaces
         vim.fn.Base16hi('ExtraWhitespace', '', vim.g.base16_gui08, '', vim.g.base16_cterm08)
+        add_match()
     end
 })
 
-vim.api.nvim_create_autocmd("WinEnter", {
+vim.api.nvim_create_autocmd({"WinEnter", "SessionLoadPost"}, {
     group = group,
-    callback = function(ev)
-        -- highlight trailing spaces
-        vim.cmd.match("ExtraWhitespace", '"\\s\\+$"')
-    end
+    callback = add_match
 })
 
 vim.cmd.colorscheme("base16-monokai")
