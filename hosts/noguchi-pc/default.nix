3587a3c6c4aa4f69758cac612e0f18bd537ea54a
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-08-06 05:38:19 +0900

    move stateVersion configuration to per-machine config

--- hosts/noguchi-pc/default.nix
+++ hosts/noguchi-pc/default.nix
@@ -155,4 +155,12 @@ in
     mode = "0400";
   };
   age.secrets."IFC.nmconnection".file = ../../secrets/IFC.nmconnection.age;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
 }
--- hosts/peregrine/default.nix
+++ hosts/peregrine/default.nix
@@ -163,4 +163,12 @@ in
 
   # XXX workaround for swaywm/sway #6962
   environment.variables.WLR_DRM_NO_MODIFIERS = "1";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
 }
