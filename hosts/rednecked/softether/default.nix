3fa23893aae55e7430c9312c60868b34b7610e6c
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-08-09 15:42:03 +0900

    rednecked: use config.services.softether.dataDir instead of hard-coded path

--- hosts/rednecked/softether/default.nix
+++ hosts/rednecked/softether/default.nix
@@ -19,8 +19,8 @@
       ./vpn_server.config
     ];
     preStart = ''
-      touch /var/lib/softether/vpnserver/vpn_server.config
-      chmod 600 /var/lib/softether/vpnserver/vpn_server.config
+      touch ${config.services.softether.dataDir}/vpnserver/vpn_server.config
+      chmod 600 ${config.services.softether.dataDir}/vpnserver/vpn_server.config
       source ${config.age.secrets.softether-server-secrets.path}
       ${pkgs.gnused}/bin/sed -e "s|@DDnsKey@|$DDnsKey|" \
                              -e "s|@AdminHashedPassword@|$AdminHashedPassword|" \
@@ -29,18 +29,18 @@
                              -e "s|@HubSecurePassword@|$HubSecurePassword|" \
                              -e "s|@UserAuthNtLmSecureHash@|$UserAuthNtLmSecureHash|" \
                              -e "s|@UserAuthPassword@|$UserAuthPassword|" \
-                             ${./vpn_server.config} >/var/lib/softether/vpnserver/vpn_server.config
-      echo en >/var/lib/softether/vpnserver/lang.config
+                             ${./vpn_server.config} >${config.services.softether.dataDir}/vpnserver/vpn_server.config
+      echo en >${config.services.softether.dataDir}/vpnserver/lang.config
 
       # tmpfs-as-root: vpnserver can't chmod symlinked log directories
-      chmod 700 ${config.modules.tmpfs-as-root.storage}/var/lib/softether/vpnserver/{packet_log,security_log,server_log}
+      chmod 700 ${config.modules.tmpfs-as-root.storage}${config.services.softether.dataDir}/vpnserver/{packet_log,security_log,server_log}
     '';
   };
 
   modules.tmpfs-as-root.persistentDirs = [
-    "/var/lib/softether/vpnserver/packet_log"
-    "/var/lib/softether/vpnserver/security_log"
-    "/var/lib/softether/vpnserver/server_log"
+    "${config.services.softether.dataDir}/vpnserver/packet_log"
+    "${config.services.softether.dataDir}/vpnserver/security_log"
+    "${config.services.softether.dataDir}/vpnserver/server_log"
   ];
 
   # see also: sslh.nix
