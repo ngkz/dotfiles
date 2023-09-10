9c05e3769217757bdb25000d2e888a8f782e6478
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-09-11 03:27:04 +0900

    lib: remove overlayPaths

--- lib.nix
+++ lib.nix
@@ -1,24 +1,9 @@
 { lib }:
 let
-  inherit (lib) lists attrsets stringToCharacters;
+  inherit (lib) stringToCharacters;
   alphabets = stringToCharacters "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   alphabets_rot13 = stringToCharacters "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 in
 {
-  overlayPaths = prev: paths: fn: builtins.foldl'
-    (
-      result: path:
-        let
-          head = builtins.head path;
-        in
-        attrsets.recursiveUpdate
-          (if !result ? "${head}" && prev ? "${head}" then result // {
-            "${head}" = prev."${head}";
-          } else result)
-          (attrsets.setAttrByPath path (fn (attrsets.getAttrFromPath path prev)))
-    )
-    { }
-    paths;
-
   rot13 = builtins.replaceStrings alphabets alphabets_rot13;
 }
