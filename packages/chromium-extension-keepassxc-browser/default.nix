bda77ffa8cb6d14accfc76e3b1a878c5363153c8
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-10-17 12:37:20 +0900

    update

--- packages/chromium-extension-keepassxc-browser/default.nix
+++ packages/chromium-extension-keepassxc-browser/default.nix
@@ -1,10 +1,10 @@
 { fetchzip, ngkz, lib, ... }:
 ngkz.buildChromiumExtension rec {
   pname = "keepassxc-browser";
-  version = "1.8.8.1";
+  version = "1.8.9";
   src = fetchzip {
     url = "https://github.com/keepassxreboot/keepassxc-browser/releases/download/${version}/keepassxc-browser_${version}_chromium.zip";
-    sha256 = "1f2pgnngigkpsfpqaz1svi2zrhczc23zzf3k6jx46rk1gkcdi5s5";
+    sha256 = "0gxclahbjn0v40knr87mf69dlkcl31jr52i7vfik5kcc69bm3wal";
     stripRoot = false;
   };
 }
