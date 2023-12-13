42a4a3cad531ffd2d7dae77e9efef00cc4e868d0
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-12-13 11:45:56 +0900

    add workaround for update-users-groups bug

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -1,6 +1,6 @@
 # configuration applied to all hosts
 
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, utils, inputs, ... }:
 let
   inherit (lib) mapAttrs filterAttrs mapAttrsToList mkIf types mkOption;
   inherit (inputs) self agenix home-manager;
@@ -225,5 +225,34 @@ in
 
     # record machine-check exception
     hardware.rasdaemon.enable = true;
+
+    # XXX temporary workaround for https://github.com/NixOS/nixpkgs/pull/273758
+    system.activationScripts.users.text =
+      with lib;
+      let
+        cfg = config.users;
+        spec = pkgs.writeText "users-groups.json" (builtins.toJSON {
+          inherit (cfg) mutableUsers;
+          users = mapAttrsToList
+            (_: u:
+              {
+                inherit (u)
+                  name uid group description home homeMode createHome isSystemUser
+                  password hashedPasswordFile hashedPassword
+                  autoSubUidGidRange subUidRanges subGidRanges
+                  initialPassword initialHashedPassword expires;
+                shell = utils.toShellPath u.shell;
+              })
+            cfg.users;
+          groups = attrValues cfg.groups;
+        });
+      in
+      lib.mkForce ''
+        install -m 0700 -d /root
+        install -m 0755 -d /home
+
+        ${pkgs.perl.withPackages (p: [ p.FileSlurp p.JSON ])}/bin/perl \
+        -w ${./update-users-groups.pl} ${spec}
+      '';
   };
 }
