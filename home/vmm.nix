4f63d3604da1795eaafe964285475fb1a1a28673
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2023-12-28 16:39:32 +0900

    replace libvirt with virtualbox and vagrant

--- home/vmm.nix
+++ home/vmm.nix
@@ -1,16 +1,6 @@
 { ... }: {
-  dconf.settings = {
-    "org/virt-manager/virt-manager" = {
-      xmleditor-enabled = true;
-    };
-
-    "org/virt-manager/virt-manager/connections" = {
-      uris = [ "qemu:///system" ];
-      autoconnect = [ "qemu:///system" ];
-    };
-
-    "org/virt-manager/virt-manager/console" = {
-      auto-redirect = false;
-    };
-  };
+  home.tmpfs-as-home.persistentDirs = [
+    ".vagrant.d"
+    "VirtualBox VMs"
+  ];
 }
--- modules/vmm.nix
+++ modules/vmm.nix
@@ -1,44 +1,12 @@
 { config, lib, pkgs, ... }: {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      swtpm.enable = true;
-      ovmf = {
-        packages = with pkgs; [ OVMFFull.fd ];
-        enable = true;
-      };
-      runAsRoot = false;
-    };
-  };
-
-  virtualisation.spiceUSBRedirection.enable = true;
-
-  users.users.user.extraGroups = [ "libvirtd" ];
+  users.users.user.extraGroups = [ "vboxusers" ];
 
   environment.systemPackages = with pkgs; [
-    libguestfs
-    virtiofsd # XXX workaround for nixpkgs#187078
+    vagrant
   ];
 
-  programs.virt-manager.enable = true;
-
-  modules.tmpfs-as-root.persistentDirs = [
-    "/var/lib/libvirt"
-  ];
-
-  systemd.services.libvirtd-config.serviceConfig = {
-    StateDirectory = lib.mkForce "";
-    ReadWritePaths = [
-      "/var/lib/libvirt"
-      "${config.modules.tmpfs-as-root.storage}/var/lib/libvirt"
-    ];
+  virtualisation.virtualbox.host = {
+    enable = true;
+    enableExtensionPack = true;
   };
-
-  # nested virtualization
-  boot.extraModprobeConfig = "options kvm_intel nested=1";
-
-  systemd.tmpfiles.rules = [
-    "a+ /home/user - - - - u:qemu-libvirtd:x"
-    "a+ ${config.modules.tmpfs-as-root.storage}/home/user - - - - u:qemu-libvirtd:x"
-  ];
 }
