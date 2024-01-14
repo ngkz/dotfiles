bc7194be316ed8eb3da2ba086ddd2b58428dd557
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2024-01-14 23:14:50 +0900

    base/tealdeer: fix the tldr warning. use home-manager tealdeer module

--- home/base/tealdeer.nix
+++ home/base/tealdeer.nix
@@ -4,25 +4,20 @@ let
   tldr_cache = "${config.xdg.cacheHome}/tealdeer/tldrpages";
 in
 {
-  # C tldr client is broken
-  home.packages = [ pkgs.tealdeer ];
-
-  xdg.configFile."tealdeeer/config.toml".text = ''
-    [updates]
-    auto_update = true
-    auto_update_interval_hours = 720
-  '';
-
-  # move a cache directory to ~/.cache/tealdeer/tldrpages
-  # tealdeer deletes the cache directory (default: ~/.cache/tealdeer) and breaks
-  # the link to the presistent storage
-  home.sessionVariables = {
-    TEALDEER_CACHE_DIR = tldr_cache;
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      updates = {
+        auto_update = true;
+      };
+      # move a cache directory to ~/.cache/tealdeer/tldrpages
+      # tealdeer deletes the cache directory (default: ~/.cache/tealdeer) and breaks
+      # the link to the presistent storage
+      directories = {
+        cache_dir = tldr_cache;
+      };
+    };
   };
-
-  home.activation.create-tealdeer-cache = hm.dag.entryAfter [ "linkGeneration" "tmpfs-as-home" ] ''
-    $DRY_RUN_CMD mkdir -p ${escapeShellArg tldr_cache}
-  '';
 
   home.tmpfs-as-home.persistentDirs = [
     ".cache/tealdeer"
