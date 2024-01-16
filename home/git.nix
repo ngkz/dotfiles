b03d65bc2c60a3baa79c09a13040630ce5536c8f
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2024-01-17 08:08:09 +0900

    home/git: move gpg-related configuration out

--- home/git.nix
+++ home/git.nix
@@ -4,12 +4,11 @@
     delta.enable = true;
     userName = "Kazutoshi Noguchi";
     userEmail = lib.ngkz.rot13 "abthpuv.xnmhgbfv+Nm0Twsg4@tznvy.pbz";
-    signing.key = "BC6DCFE03513A9FA4F55D70206B8106665DD36F3";
     extraConfig = {
       init.defaultBranch = "main";
       diff.tool = "nvimdiff";
       merge.tool = "nvimdiff";
-      tag.gpgSign = true;
     };
   };
+  # See also: gnupg.nix
 }
