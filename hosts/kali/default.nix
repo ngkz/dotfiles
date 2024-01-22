b82211f45a037572efa0d007659ea3773c60e6ac
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2024-01-23 05:57:56 +0900

    hosts/kali: install libreoffice

--- hosts/kali/default.nix
+++ hosts/kali/default.nix
@@ -19,7 +19,8 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
+    libreoffice
   ];
 
   # Let Home Manager install and manage itself.
