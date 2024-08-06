6fceec81f13d1f735d2c62fc28a689219dfca19b
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2024-08-07 06:21:26 +0900

    modules/profiles/intel-wifi: install iw

--- modules/profiles/intel-wifi.nix
+++ modules/profiles/intel-wifi.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # power saving
   boot.extraModprobeConfig = ''
     options iwlwifi power_save=1 uapsd_disable=0
     options iwlmvm power_scheme=3
   '';
+
+  environment.systemPackages = with pkgs; [
+    iw
+  ];
 }
