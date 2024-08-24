562da1e8ec249044e263e062436388bb9ffa5cd7
Kazutoshi Noguchi <kn@f2l.cc>
2024-08-24 18:56:42 +0900

    modules/print-and-scan: add Canon TS5430 printer

--- modules/print-and-scan.nix
+++ modules/print-and-scan.nix
@@ -4,16 +4,22 @@
   # printing
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ epson-escpr2 ];
+    drivers = with pkgs; [ epson-escpr2 cnijfilter2 ];
   };
   hardware.printers = {
-    ensureDefaultPrinter = "EPSON-EW-M754T-Series";
+    ensureDefaultPrinter = "Canon-TS5400-Series";
     ensurePrinters = [
       {
         description = "EPSON EW-M754T Series";
         deviceUri = "dnssd://EPSON%20EW-M754T%20Series._ipp._tcp.local/?uuid=cfe92100-67c4-11d4-a45f-381a526218ab";
         model = "epson-inkjet-printer-escpr2/Epson-EW-M754T_Series-epson-escpr2-en.ppd";
         name = "EPSON-EW-M754T-Series";
+      }
+      {
+        description = "Canon TS5400 Series";
+        deviceUri = "dnssd://Canon%20TS5400%20series._ipp._tcp.local/?uuid=00000000-0000-1000-8000-0018d8806c8b";
+        model = "canonts5400.ppd";
+        name = "Canon-TS5400-Series";
       }
     ];
   };
