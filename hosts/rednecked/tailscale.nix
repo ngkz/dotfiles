4eed71f62af76dc901bce4ba619478747d774739
Kazutoshi Noguchi <kn@f2l.cc>
2024-08-31 13:36:50 +0900

    hosts/rednecked/tailscale: enable udp optimization on ipv6 default gateway. remove decomissioned 192.168.70.0/24 network from subnet routing

--- hosts/rednecked/tailscale.nix
+++ hosts/rednecked/tailscale.nix
@@ -8,7 +8,7 @@
   # enable subnet router and exit node (route all traffic)
   services.tailscale.useRoutingFeatures = "both";
   services.tailscale.extraSetFlags = [
-    "--advertise-routes=${config.hosts.rednecked.network.lan_prefix}.0/24,192.168.33.0/24,192.168.70.0/24" # allow routing traffic from other nodes to lan
+    "--advertise-routes=${config.hosts.rednecked.network.lan_prefix}.0/24,192.168.33.0/24" # allow routing traffic from other nodes to lan
     "--accept-routes" # route traffic to other lan
     "--advertise-exit-node" # allow allow routing traffic through this node
   ];
@@ -20,6 +20,7 @@
     onState = [ "routable" ];
     script = ''
       ${lib.getExe pkgs.ethtool} -K wan_pppoe rx-udp-gro-forwarding on rx-gro-list off
+      ${lib.getExe pkgs.ethtool} -K wan_hgw rx-udp-gro-forwarding on rx-gro-list off
     '';
   };
 
