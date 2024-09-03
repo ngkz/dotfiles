7fdcee8e1355a04445f29dfeb56d2dde1b97f8f0
Kazutoshi Noguchi <kn@f2l.cc>
2024-09-04 04:40:10 +0900

    update

--- home/doom-emacs/default.nix
+++ home/doom-emacs/default.nix
@@ -23,8 +23,8 @@ in
     "emacs".source = pkgs.fetchFromGitHub {
       owner = "doomemacs";
       repo = "doomemacs";
-      rev = "84230a437dfca913ba94c39f7a61c41d24a7cfbd";
-      hash = "sha256-XztBie2wCNxQzVtiKfNmtv34biyIDrbDn5aHQXOHQhs=";
+      rev = "559e5b6a966fa82bf8322f89d78a00ef4181812a";
+      hash = "sha256-VolWh2VkVydYmOhmALxODhDgUIpIFt3iY7lFGFyHJFQ=";
     };
   };
 
