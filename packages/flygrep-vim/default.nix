7e1a7d896c94148951c35b25d5d4448f556ded59
Kazutoshi Noguchi <kn@f2l.cc>
2025-02-05 09:06:36 +0900

    update

--- packages/flygrep-vim/default.nix
+++ packages/flygrep-vim/default.nix
@@ -7,7 +7,7 @@ buildVimPlugin {
   src = fetchFromGitHub {
     owner = "wsdjeg";
     repo = "FlyGrep.vim";
-    rev = "221e4b2de8af3ad4190610bacee6f7afc43e2194";
-    hash = "sha256-ZLV8NbyJrzNYw8vE4pIlkO+jwM4id3uN1zO1aripb/M=";
+    rev = "54bd78ee9e58406fd1698b5ffe51518742238df3";
+    hash = "sha256-hc0blsSQifHLOFG2dOXyvfUqzo3Ezmlmbp+UV+igDGE=";
   };
 }
