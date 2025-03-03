eccb1353b5a297d2a8025a4810e8942dec3fc81d
Kazutoshi Noguchi <kn@f2l.cc>
2025-03-03 18:34:08 +0900

    update

--- packages/sarasa-term-j-nerd-font/default.nix
+++ packages/sarasa-term-j-nerd-font/default.nix
@@ -1,13 +1,13 @@
 { lib, fetchzip }:
 
 let
-  version = "1.0.28-0";
+  version = "1.0.29-0";
 in
 fetchzip {
   name = "sarasa-term-j-nerd-font-${version}";
 
   url = "https://github.com/jonz94/Sarasa-Gothic-Nerd-Fonts/releases/download/v${version}/sarasa-term-j-nerd-font.zip";
-  sha256 = "sha256-brjfmvHd93EwiFdGvnvBYHrbNQbHbKlLsM9A5TYUPgw=";
+  sha256 = "sha256-Aw/opLth78Lsrdh9mluh0RI7w0Lu1KEInJ2Atww7dR8=";
 
   stripRoot = false;
 
