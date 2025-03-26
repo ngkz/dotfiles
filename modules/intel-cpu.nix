a58a824791a9a5446d4d33ee4ef76bcd040a7b39
Kazutoshi Noguchi <kn@f2l.cc>
2025-03-26 21:23:56 +0900

    modules/intel-cpu: use kaby-lake profile

--- modules/intel-cpu.nix
+++ modules/intel-cpu.nix
@@ -4,28 +4,19 @@ let
   inherit (lib) mkOption types mkIf;
 in
 {
-  imports = with nixos-hardware.nixosModules; [
-    common-cpu-intel
+  imports = [
+    "${nixos-hardware}/common/cpu/intel/kaby-lake"
   ];
 
-  options.profiles.intel-cpu.enableGPUPowerSaving = mkOption {
-    type = types.bool;
-    default = true;
-    description = "Enable i915 power saving options.";
-  };
+  hardware.enableRedistributableFirmware = true;
+  boot.kernelModules = [ "kvm-intel" ];
 
-  config = {
-    hardware.enableRedistributableFirmware = true;
-    boot.kernelModules = [ "kvm-intel" ];
+  environment.systemPackages = with pkgs; [
+    intel-gpu-tools # intel_gpu_top
+  ];
 
-    environment.systemPackages = with pkgs; [
-      intel-gpu-tools # intel_gpu_top
-    ];
-
-    # power saving
-    boot.extraModprobeConfig = mkIf config.profiles.intel-cpu.enableGPUPowerSaving ''
-      options i915 enable_fbc=1 enable_psr=2 enable_guc=2 enable_psr2_sel_fetch=1
-      options drm vblankoffdelay=1
-    '';
-  };
+  # power saving
+  boot.extraModprobeConfig = ''
+    options drm vblankoffdelay=1
+  '';
 }
