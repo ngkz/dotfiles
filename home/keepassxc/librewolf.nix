9b16439f2683a6667fb2dd90b25c3a5b9040d0c3
Kazutoshi Noguchi <kn@f2l.cc>
2025-03-28 08:28:45 +0900

    home/keepassxc/librewolf: fix native-messaging-hosts json path

--- home/keepassxc/librewolf.nix
+++ home/keepassxc/librewolf.nix
@@ -1,7 +1,7 @@
 # KeePassXC Firefox/LibreWolf integration
 { pkgs, ... }:
 {
-  home.file.".mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".source = pkgs.substituteAll {
+  home.file.".librewolf/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".source = pkgs.substituteAll {
     src = ./org.keepassxc.keepassxc_browser.json;
     inherit (pkgs) keepassxc;
   };
